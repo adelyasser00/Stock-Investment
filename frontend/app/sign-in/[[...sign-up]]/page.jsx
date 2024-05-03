736dd8c6f0c90eb1f100b02cad828a045f458088
hayamhiba <es-hayam.ibrahim2023@alexu.edu.eg>
2024-05-03 21:12:24 +0300

    add debugging consoles

--- frontend/app/sign-in/[[...sign-up]]/page.jsx
+++ frontend/app/sign-in/[[...sign-up]]/page.jsx
@@ -1,5 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
 import '../../css/clerk.css';
 export default function Page() {
+    console.log("inside sign in")
     return <SignIn className='mainContainer'/>;
 }
