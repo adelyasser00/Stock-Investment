3bfc81f5c3164e8cf5e14207e2ba31411f1d5a18
hayamhiba <es-hayam.ibrahim2023@alexu.edu.eg>
2024-05-06 17:50:40 +0300

    first integration

--- frontend/app/api/webhooks/test/route.ts
+++ frontend/app/api/webhooks/test/route.ts
@@ -3,7 +3,7 @@ import { getUserById, addToWatchlist,removeFromWatchlist ,addInvestment, removeI
 import mongoose, { Mongoose, Schema } from "mongoose";
 
 export async function POST(request) {
-  console.log('inside test file');
+  console.log(request);
 
   // Assuming you want to use a hardcoded clerkId and companyId for testing
   const clerkId = "user_2g30qZw3lkKOlnS953WKjADYzHS"; // This should be the clerkId you intend to use
@@ -15,15 +15,15 @@ export async function POST(request) {
     };
 
   try {
-    // const added = await addToWatchlist(clerkId, companyId);
-    //   console.log('User added:', added);
+    const added = await addToWatchlist(clerkId, companyId);
+      console.log('User added:', added);
 
-    // const removed = await removeFromWatchlist(clerkId, companyId);
-    //  console.log('User added:', removed);
+    const removed = await removeFromWatchlist(clerkId, companyId);
+     console.log('User added:', removed);
 
-// console.log("before creating investment")
-//      const user_nvested = await addInvestment(clerkId, invested);
-//      console.log('User invested:', user_nvested);
+console.log("before creating investment")
+     const user_nvested = await addInvestment(clerkId, invested);
+     console.log('User invested:', user_nvested);
 const stockId = '6637ef8081b4f0d3b76f17dc';
   // const stockId = new mongoose.Types.ObjectId('6637a39f85532cf8d2042ea4'); // Replace 'yourExampleStockIdHere' with an actual stock ID string
 
